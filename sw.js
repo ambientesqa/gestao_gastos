835dd6bd24f66954d284d3d5090ce3530fc8dd13
ambientesqa <orange.incorporation15@gmail.com>
2026-09-20 13:26:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,30 @@
+const CACHE_NAME = 'gestao-gastos-v2'; // Altere esta versão (ex: v3, v4) sempre que atualizar o app na hospedagem
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Força o novo service worker a assumir o controle imediatamente
   e.waitUntil(
-    caches.open('gestao-gastos-cache').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         './',
         './index.html'
       ]);
     })
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Remove os caches antigos para liberar espaço e garantir dados novos
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
